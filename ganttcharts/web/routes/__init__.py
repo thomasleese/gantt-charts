1af54ef0398e0e691fc119962b72dc4e69542a3a
Tom Leese <inbox@thomasleese.me>
2015-09-20 15:42:05 +0100

    Move frontend pages into blueprint

--- ganttcharts/web/routes/__init__.py
+++ ganttcharts/web/routes/__init__.py
@@ -1,4 +1,6 @@
 from .api import blueprint as api_blueprint
+from .frontend import blueprint as frontend_blueprint
 
 def register(app):
     app.register_blueprint(api_blueprint)
+    app.register_blueprint(frontend_blueprint)
