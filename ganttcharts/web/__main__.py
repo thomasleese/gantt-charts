40bbb9ca0dbd2081cdf1dfae280bcf72a1f7b59b
Thomas Leese <inbox@thomasleese.me>
2015-11-09 12:54:33 +0000

    Force local server to run on port 5000

--- ganttcharts/web/__main__.py
+++ ganttcharts/web/__main__.py
@@ -1,11 +1,4 @@
-from argparse import ArgumentParser
-
 from ganttcharts.web import app
 
 
-parser = ArgumentParser(description='Web server useful for testing.')
-parser.add_argument('--host', default='0.0.0.0')
-parser.add_argument('--port', type=int, default=5000)
-args = parser.parse_args()
-
-app.run(host=args.host, port=args.port, debug=True)
+app.run(port=5000, debug=True)
