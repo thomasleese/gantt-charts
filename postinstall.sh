bedd7917858f9c67c8505b11dca05b871e23fd86
Thomas Leese <inbox@thomasleese.me>
2015-11-09 13:36:47 +0000

    Use --only instead of --dev

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,7 @@ bower install
 
 pushd ganttcharts/web/static/bower_components/bootstrap
 npm install
-npm install --dev  # for grunt stuff
+npm install --only=dev  # for grunt stuff
 popd
 
 cp ganttcharts/web/static/bootstrap_variables.scss ganttcharts/web/static/bower_components/bootstrap/scss/_variables.scss
