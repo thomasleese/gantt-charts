4b2bac490cda6030ed3e7c8859d46c59dcf6d1d2
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-03-29 09:11:10 +0100

    Include Bootstrap distribution

--- postinstall.sh
+++ postinstall.sh
@@ -3,14 +3,3 @@
 set -e
 
 bower install --quiet
-
-pushd ganttcharts/web/static/bower_components/bootstrap
-npm install --silent
-npm install --only=dev --silent  # for grunt stuff
-popd
-
-cp ganttcharts/web/static/bootstrap_variables.scss ganttcharts/web/static/bower_components/bootstrap/scss/_variables.scss
-
-pushd ganttcharts/web/static/bower_components/bootstrap
-grunt dist
-popd
